9ef456c06f6ae02b7a7ccf0880c0ac5b4d441529
Muffin <muffin@CPE1704TKS.local>
2013-05-13 12:14:51 +0100

    Separated out matrix printing code

--- elimination_gold.cu
+++ elimination_gold.cu
@@ -1,9 +1,22 @@
 #include "elimination_gold.h"
 
+void print_matrix(float *a, float *b, int n) {
+#define element(_x, _y) (*(a + ((_y) * (n) + (_x))))
+	unsigned int i, j;
+	for (j = 0; j < n; j++) {
+		printf("[");
+		for (i = 0; i < n; i++) {
+			printf("%6.3f ", element(i, j));
+		}
+		printf("| %6.3f ]\n", b[j]);
+	}
+#undef element
+}
+
 void elimination_gold(float *a, float *b, int n) {
 #define element(_x, _y) (*(a + ((_y) * (n) + (_x))))
 	unsigned int xx, yy, rr;
-	unsigned int i, j;
+
 	float c;
 
 	for (yy = 0; yy < n; yy++) {
@@ -16,13 +29,7 @@ void elimination_gold(float *a, float *b, int n) {
 
 		// Print out matrix
 		printf("Matrix (Stage 1; Column %d):\n", yy);
-		for (j = 0; j < n; j++) {
-			printf("[");
-			for (i = 0; i < n; i++) {
-				printf("%6.3f ", element(i, j));
-			}
-			printf("| %6.3f ]\n", b[j]);
-		}
+		print_matrix(a, b, n);
 
 		// Make all other values in the pivot column be zero
 		for (rr = 0; rr < n; rr++) {
@@ -36,13 +43,7 @@ void elimination_gold(float *a, float *b, int n) {
 
 		// Print out matrix
 		printf("Matrix (Stage 2; Column %d):\n", yy);
-		for (j = 0; j < n; j++) {
-			printf("[");
-			for (i = 0; i < n; i++) {
-				printf("%6.3f ", element(i, j));
-			}
-			printf("| %6.3f ]\n", b[j]);
-		}
+		print_matrix(a, b, n);
 	}
 #undef element
 }
